2267d7a241d06db748b83324a6438ef0385edcb3
VASYLKOVSKYI <vovavasilkovsky7777@gmail.com>
2016-12-04 22:34:46 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -72,15 +72,55 @@ export class AppComponent {
     "title": "qui eius qui autem sed",
     "url": "http://placehold.it/600/51aa97",
     "thumbnailUrl": "http://placehold.it/150/15c072"
+  },
+  {
+    "albumId": 1,
+    "id": 10,
+    "title": "beatae et provident et ut vel",
+    "url": "http://placehold.it/600/810b14",
+    "thumbnailUrl": "http://placehold.it/150/980cc2"
+  },
+  {
+    "albumId": 1,
+    "id": 11,
+    "title": "nihil at amet non hic quia qui",
+    "url": "http://placehold.it/600/1ee8a4",
+    "thumbnailUrl": "http://placehold.it/150/e65eee"
+  },
+  {
+    "albumId": 1,
+    "id": 12,
+    "title": "mollitia soluta ut rerum eos aliquam consequatur perspiciatis maiores",
+    "url": "http://placehold.it/600/66b7d2",
+    "thumbnailUrl": "http://placehold.it/150/bc9589"
+  },
+  {
+    "albumId": 1,
+    "id": 13,
+    "title": "repudiandae iusto deleniti rerum",
+    "url": "http://placehold.it/600/197d29",
+    "thumbnailUrl": "http://placehold.it/150/f777f7"
+  },
+  {
+    "albumId": 1,
+    "id": 14,
+    "title": "est necessitatibus architecto ut laborum",
+    "url": "http://placehold.it/600/61a65",
+    "thumbnailUrl": "http://placehold.it/150/8fa5e0"
+  },
+  {
+    "albumId": 1,
+    "id": 15,
+    "title": "harum dicta similique quis dolore earum ex qui",
+    "url": "http://placehold.it/600/f9cee5",
+    "thumbnailUrl": "http://placehold.it/150/ae926c"
+  },
+  {
+    "albumId": 1,
+    "id": 16,
+    "title": "iusto sunt nobis quasi veritatis quas expedita voluptatum deserunt",
+    "url": "http://placehold.it/600/fdf73e",
+    "thumbnailUrl": "http://placehold.it/150/dc71a1"
   }
   ];
 }}
-
-export class HttpSample {
-    result: Object;
-    constructor(http: Http) {
-        this.result = {photos:[]};
-        http.get('./photos.json').map((res: Response) => res.json()).subscribe(res => this.result = res);
-    }
-}
-
